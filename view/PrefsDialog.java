397762f48f659082f397ee64ba1abe0f5bead620
JadeTiger88 <jadetiger88@gmail.com>
2015-05-09 15:56:27 -0700

    Make Preferences Diaglog Box Prettier and Re-structure the PrefsDialog.java file

--- view/PrefsDialog.java
+++ view/PrefsDialog.java
@@ -1,18 +1,26 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.border.Border;
 
 public class PrefsDialog extends JDialog {
 
@@ -23,18 +31,32 @@ public class PrefsDialog extends JDialog {
 	private JTextField userNameField;
 	private JPasswordField pwdField;
 	private PrefsListener prefsListener;
+	private JPanel controlPanel;
+	private JPanel buttonPanel;
 
 	public PrefsDialog(JFrame parent) {
 		super(parent, "Preferences", false);
-		setSize(400, 300);
-		setLocationRelativeTo(parent);
+		setPrefsDialogBoxDisplayParameters(parent);
+		createComponents();
+		layoutComponents();
+		populateButtonActionListeners();
+	}
 
+	private void createComponents() {
 		userNameField = new JTextField(20);
 		pwdField = new JPasswordField(20);
 		okBtn = new JButton("ok");
 		cancelBtn = new JButton("cancel");
 		spinnerNumberModel = new SpinnerNumberModel(3306, 0, 9999, 1);
 		spinner = new JSpinner(spinnerNumberModel);
+	}
+
+	private void setPrefsDialogBoxDisplayParameters(JFrame parent) {
+		setSize(500, 180);
+		setLocationRelativeTo(parent);
+	}
+
+	private void populateButtonActionListeners() {
 		okBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				Integer port = (Integer) spinner.getValue();
@@ -53,13 +75,43 @@ public class PrefsDialog extends JDialog {
 				setVisible(false);
 			}
 		});
-		;
-
-		layoutComponent();
 	}
 
-	private void layoutComponent() {
-		setLayout(new GridBagLayout());
+	private void layoutComponents() {
+
+		controlPanel = new JPanel();
+		buttonPanel = new JPanel();
+
+		// Set layout
+		setLayout(new BorderLayout());
+		controlPanel.setLayout(new GridBagLayout());
+		buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+		// Set border
+		int spacer = 15; 
+		Border spaceBorder = BorderFactory.createEmptyBorder(spacer, spacer, spacer, spacer);
+		Border tileBorder = BorderFactory.createTitledBorder("SQL Database Setting");
+		controlPanel.setBorder(BorderFactory.createCompoundBorder(spaceBorder, tileBorder));
+
+		// Add Panels and components
+		addPanelsToPrefsDialog();
+		addComponentsToControlPanel();
+		addComponentsToButtonPanel();
+	}
+
+	private void addComponentsToButtonPanel() {
+		// Button Panel
+		Dimension btnSize = cancelBtn.getPreferredSize();
+		okBtn.setPreferredSize(btnSize);
+		buttonPanel.add(okBtn);
+		buttonPanel.add(cancelBtn);
+	}
+
+	private void addComponentsToControlPanel() {
+		// Add components to controlPanel
+		Insets rightPadding = new Insets(0, 0, 0, 15);
+		Insets noPadding = new Insets(0, 0, 0, 0);
+
 		GridBagConstraints gc = new GridBagConstraints();
 		gc.weightx = 1;
 		gc.weighty = 1;
@@ -69,35 +121,43 @@ public class PrefsDialog extends JDialog {
 		// First row
 		gc.gridy++;
 		gc.gridx = 0;
-		add(new JLabel("Use Name:"), gc);
+		gc.anchor = GridBagConstraints.EAST;
+		gc.insets = rightPadding;
+		controlPanel.add(new JLabel("Use Name:"), gc);
 
 		gc.gridx = 1;
-		add(userNameField, gc);
+		gc.anchor = GridBagConstraints.WEST;
+		gc.insets = noPadding;
+		controlPanel.add(userNameField, gc);
 
 		// 2nd row
 		gc.gridy++;
 		gc.gridx = 0;
-		add(new JLabel("Password: "), gc);
+		gc.anchor = GridBagConstraints.EAST;
+		gc.insets = rightPadding;
+		controlPanel.add(new JLabel("Password: "), gc);
 
 		gc.gridx = 1;
-		add(pwdField, gc);
+		gc.anchor = GridBagConstraints.WEST;
+		gc.insets = noPadding;
+		controlPanel.add(pwdField, gc);
 
 		// 3rd
 		gc.gridy++;
 		gc.gridx = 0;
-		add(new JLabel("port: "), gc);
+		gc.anchor = GridBagConstraints.EAST;
+		gc.insets = rightPadding;
+		controlPanel.add(new JLabel("port: "), gc);
 
 		gc.gridx = 1;
-		add(spinner, gc);
+		gc.anchor = GridBagConstraints.WEST;
+		gc.insets = noPadding;
+		controlPanel.add(spinner, gc);
+	}
 
-		// 4th
-		gc.gridy++;
-		gc.gridx = 0;
-		add(okBtn, gc);
-
-		gc.gridx = 1;
-		add(cancelBtn, gc);
-
+	private void addPanelsToPrefsDialog() {
+		add(controlPanel, BorderLayout.CENTER);
+		add(buttonPanel, BorderLayout.SOUTH);
 	}
 
 	public void setPreferencesListener(PrefsListener prefsListener) {
