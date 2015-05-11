517e2f8fd6dcb946692e212a1fa5c6d7417f8e39
JadeTiger88 <jadetiger88@gmail.com>
2015-05-11 10:44:44 -0700

    Add Tree Selection Listener

--- view/MessagePanel.java
+++ view/MessagePanel.java
@@ -5,7 +5,10 @@ import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeSelectionModel;
 
 public class MessagePanel extends JPanel {
 
@@ -13,8 +16,21 @@ public class MessagePanel extends JPanel {
 
 	public MessagePanel() {
 		serverTree = new JTree(createTreeNode());
+
+		serverTree.getSelectionModel().setSelectionMode(
+				TreeSelectionModel.SINGLE_TREE_SELECTION);
 		setLayout(new BorderLayout());
 		add(new JScrollPane(serverTree), BorderLayout.CENTER);
+
+		serverTree.addTreeSelectionListener(new TreeSelectionListener() {
+			public void valueChanged(TreeSelectionEvent arg0) {
+				DefaultMutableTreeNode node = (DefaultMutableTreeNode) serverTree
+						.getLastSelectedPathComponent();
+				String location = (String) node.getUserObject();
+				System.out.println(location);
+			}
+
+		});
 
 	}
 
